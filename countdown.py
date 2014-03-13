a583fc76160d47f9d3c247b11ba03cb0138f4871
bishop <miraclecome@gmail.com>
2014-03-13 09:22:26 +0800

    first commit

---
+++ countdown.py
@@ -0,0 +1,22 @@
+
+class Countdown(object):
+
+    counter = 5
+
+    def count(self):
+        if self.counter == 0:
+            reactor.stop()
+        else:
+            print self.counter, '...'
+            self.counter -= 1
+            reactor.callLater(1, self.count)
+
+#from twisted.internet import pollreactor
+#pollreactor.install()
+from twisted.internet import reactor
+
+reactor.callWhenRunning(Countdown().count)
+
+print 'Start!'
+reactor.run()
+print 'Stop!'
